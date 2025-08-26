88a2630d49a2637b9ca5961bd3d30592c3fd336a
Anatoly Levitsky <levitsky@element-soft.com>
2025-08-26 10:33:30 +0300

    init commit

---
+++ src/main/java/ru/levitsky/unicongen/UnicongenApplication.java
@@ -0,0 +1,13 @@
+package ru.levitsky.unicongen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicongenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnicongenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/levitsky/unicongen/UnicongenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.levitsky.unicongen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicongenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
